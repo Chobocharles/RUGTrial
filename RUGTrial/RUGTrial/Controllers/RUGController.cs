13da4461f796b64695b1cf14005dd329af5b8f82
Charles <chobocharles@gmail.com>
2024-03-24 14:20:34 -0400

    Basic file structures

---
+++ RUGTrial/RUGTrial/Controllers/RUGController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RUGTrial.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RUGController : ControllerBase
+    {
+        public async Task DefaultMethod()
+        {
+            return;
+        }
+    }
+}
