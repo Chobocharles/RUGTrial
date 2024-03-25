527afae30982bc0f9df6c5357a62a6bb07aa7079
Charles <chobocharles@gmail.com>
2024-03-25 06:52:13 -0400

    Git bug fix

--- RUGTrial/RUGTrial/Program.cs
+++ RUGTrial/RUGTrial/Program.cs
@@ -12,7 +12,6 @@ builder.Services.AddSingleton<IRUGService, RUGService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
