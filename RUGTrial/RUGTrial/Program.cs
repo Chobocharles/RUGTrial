fbc7127271ca05502f21f7ce071ddab97ce4a338
Charles <chobocharles@gmail.com>
2024-03-25 07:03:35 -0400

    Kicking off build

--- RUGTrial/RUGTrial/Program.cs
+++ RUGTrial/RUGTrial/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSingleton<IRUGService, RUGService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
